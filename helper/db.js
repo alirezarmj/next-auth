b3e9aed7621ed00d92a1245362fc5790c6740ece
Rashidi-Moghadam <a.rashidi.m78@gmail.com>
2023-04-16 10:41:21 +0330

    first commit

--- helper/db.js
+++ helper/db.js
@@ -1,6 +1,6 @@
 import { MongoClient } from "mongodb";
 
 export async function connectToDatabase() {
-    const client = await MongoClient.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.ctkodcc.mongodb.net/?retryWrites=true&w=majority`);
+    const client = await MongoClient.connect("mongodb+srv://javid:DjtztDJzNVrswGmF@cluster0.ctkodcc.mongodb.net/?retryWrites=true&w=majority");
     return client
 }
